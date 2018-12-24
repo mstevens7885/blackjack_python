2ad1b70202392a7c8d5be2fdf730bafe34ecf040
mstevens7885 <43992790+mstevens7885@users.noreply.github.com>
2018-12-24 09:46:13 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -7,8 +7,15 @@ VALUES = {"Two":2, "Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8,
 
 #Create a Card class
 class Card():
+    '''
+    DESCRIPTION: Card object used to represent a playing card
+    INPUTS: the card's suit and the card's rank
+    ATTRIBUTES: suit, rank
+    METHODS: None
+    SPECIAL FUNCTIONS: __str__()
+    '''
     #Define the two attributes of a card: suit and rank
-    def __init__(self, suit, rank):
+    def __init__(self, suit: str, rank: str):
         #Initialize suit and rank variables as part of the Card class, with the suit and rank attributes passed to the class
         self.suit = suit
         self.rank = rank   
@@ -19,61 +26,115 @@ class Card():
 
 #Create a Deck class
 class Deck():
+    '''
+    DESCRIPTION: Deck object used to represent a deck of cards, populated by card objects that are created from the global variables
+    INPUTS: None
+    ATTRIBUTES: cards
+    METHODS: shuffle_cards(), draw_card() 
+    SPECIAL FUNCTIONS: __str__(), __len__()
+    '''
     #Initialize the Deck object by creating the 52 card deck out of Card objects
     def __init__(self):   
-        #Initialize the deck variable as part of the Deck class, which will be loaded with the 52 cards of type Card
-        self.deck = []
+        #Initializes the cards variable, which is a list that will contain Card objects
+        self.cards = []
 
         #For each suit in the GLOBAL VARIABLE suits
         for suit in SUITS:
             #For each suit in the GLOBAL VARIABLE suits
             for rank in RANKS:
                 #Add a Card object of suit and rank to the self.deck variable
-                self.deck.append(Card(suit, rank))
+                self.cards.append(Card(suit, rank))
     
-    #Create a string representation of self.deck to report back when the deck is called as a string
+    #Create a string representation of self.cards to report back when the deck is called as a string
     def __str__(self):
         #Create an empty string for the deck contents
         deck_contents = ""
 
-        #For each card in self.deck.  The card variable is just the variable used for iteration of all self.deck Card objects in this case.
-        for card in self.deck:
+        #For each Card object in self.deck.  The card variable is just the variable used for iteration of all self.deck Card objects in this case.
+        for card in self.cards:
             #Add the string definition of each Card object to the deck contents string.  The string definition is: f"{Card.rank} of {Card.suit}"
             deck_contents += card.__str__() + "\n"
         #Return the deck_contents string
         return deck_contents
     
-    #Create a length representation of self.deck to report back when the length of the deck is requested
+    #Create a length representation of self.cards to report back when the length of the deck is requested
     def __len__(self):
-        return len(self.deck)
+        return len(self.cards)
     
-    #Use the random.shuffle() method to shuffle the list of cards in place
+    #Use the random.shuffle() method to shuffle the deck object, which is a list of card objects, in place
     def shuffle_cards(self):
-        random.shuffle(self.deck)
+        '''
+        DESCRIPTION: Shuffle the self.cards object list in place
+        INPUT: None
+        OUTPUT: None
+        '''
+        random.shuffle(self.cards)
 
-    #Use the pop() built in method for lists to remove the 
+    #Use the pop() built in method for lists to remove a card object from the deck list and return the card object
     def draw_card(self):
-        #Remove the card from the end of the list, which would be the "top" card.  This isn't really necessary since the order is randomized, but it's fun.
-        return self.deck.pop(-1)
+        '''
+        DESCRIPTION: Remove a card object from the self.cards list and return the object
+        INPUT: None
+        OUTPUT: Returns the card object at the end of the self.cards list
+        '''
+        try:
+            #Remove the card from the end of the list, which would be the "top" card.  This isn't really necessary since the order is randomized, but it's fun.
+            return self.cards.pop(-1)
+        except IndexError:
+            print("There are no cards to draw")
 
 #Create a Hand class
 class Hand():
+    '''
+    DESCRIPTION: Hand object used to represent a player's hand of cards
+    INPUTS: None
+    ATTRIBUTES: hand, hand_value, aces
+    METHODS: None
+    SPECIAL FUNCTIONS: __str__()
+    '''
     #Initialize the Hand object by creating an empty hand with no value
     def __init__(self):
         #Initialize the hand variable to hold a list of cards as part of the Hand class
         self.hand = []
+        #Initialize the hand_value variable to track the value of the hand
         self.hand_value = 0
+        #Initialize the aces variable to track the number of aces
         self.aces = 0
+
+    def __str__(self):
+        hand_contents = ""
+
+        for card in self.hand:
+            hand_contents += card.__str__() + "\n"
+        return hand_contents
+
+    #Create a length representation of self.hand to report back when the length of the hand is requested
+    def __len__(self):
+        return len(self.hand)
     
     def draw_hand(self, number_to_draw, deck: Deck):
-        self.number_to_draw = number_to_draw
+        '''
+        DESCRIPTION: Removes a specified number of card objects from the deck object, and adds them to the hand object
+        INPUT: the number of cards to draw, the deck object to draw the card objects from
+        OUTPUT: None
+        '''
+        #Loop through the specified number_to_draw range
+        #############################################################LEFT OFF HERE
         for _ in range(number_to_draw):
-            card = deck.draw_card()
-            self.hand.append(card)
-            self.hand_value += VALUES[card.rank]
-            if card.rank == "Ace":
-                self.aces += 1
-    
+            try:
+                #Set the card variable equal to the returned value of the draw_card() method from the Deck class, which is a Card object
+                card = deck.draw_card()
+                #Add the card object to the hand object
+                self.hand.append(card)
+                #Add the card's value to the hand's value, which is determined from the global VALUES dictionary that contains each rank's value
+                self.hand_value += VALUES[card.rank]
+                #If the card's rank is "Ace", add to the self.aces counter
+                if card.rank == "Ace":
+                    self.aces += 1
+            except:
+                print("The deck has no more cards!")
+                break
+            
     def add_card(self, card: Card):
         self.hand.append(card)
         self.hand_value += VALUES[card.rank]
@@ -84,17 +145,11 @@ class Hand():
         while (self.hand_value > 21) and (self.aces > 0):
             self.hand_value -= 10
             self.aces -= 1
-    
-    def __str__(self):
-        hand_contents = ""
 
-        for card in self.hand:
-            hand_contents += card.__str__() + "\n"
-        return hand_contents
 
 class Chips():
     def __init__(self):
-        self.total_bank = int(input("Enter starting pot value: "))
+        self.total_bank = int(input("Enter starting bet value: "))
         self.bet = 0
 
     def win_bet(self):
@@ -102,21 +157,41 @@ class Chips():
     
     def lose_bet(self):
         self.total_bank -= self.bet
+    
+    def __str__(self):
+        return f"Value of the pot is: {self.total_bank}"
+
+def make_bet(Chips):
+    while True:
+        try:
+            Chips.bet = int(input("Enter your bet: "))
+        except:
+            print("Please enter a positive integer value.")
+        
+    return Chips.bet
 
 
 
-##### TEST #####
+
+
+##### TEST DECK #####
 myDeck = Deck()
 myDeck.shuffle_cards()
-#print(myDeck)
+print(f"Your Deck: \n{myDeck}")
+
 
 myHand = Hand()
-myHand.draw_hand(2, myDeck)
+myHand.draw_hand(54, myDeck)
 #myHand.add_card(myDeck.draw_card())
 #testAce = Card("Hearts", "Ace")
 #myHand.add_card(testAce)
 #myHand.add_card(myDeck.draw_card())
 myHand.check_aces()
+
+#myChips = Chips()
+#myChips.bet = 100
+#myChips.win_bet()
+#print(myChips)
 
 print(myHand)
 print(f"Value: {myHand.hand_value}")
